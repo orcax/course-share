eba9713abe0a483b97496a7015019350e3456d4b
orcax <orcax@192.168.1.102>
2014-01-24 17:46:36 +0800

    version 1.01

--- src/org/tjsse/courseshare/controller/ImageController.java
+++ src/org/tjsse/courseshare/controller/ImageController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.tjsse.courseshare.bean.Image;
-import org.tjsse.courseshare.service.CoursewareService;
 import org.tjsse.courseshare.service.ImageService;
 import org.tjsse.courseshare.util.Config;
 
